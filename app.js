f986c22ebce1fc45ebf077407c1f3d3ab0bfba4c
shororen <shororen@post.bgu.ac.il>
2019-05-23 17:21:40 +0300

    commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/login', function (req, res) { // Login and Hello
 // {"userName": String, "password": String, "passQuestion":{"question": String, "answer": String},\
 // "city": String, "country": String, "e-mail": String, "firstName" :String, "lastName": String, "fieldsOfInterests": []}
 
-app.use('/private', function(req, res, next){
+app.use('/private', function (req, res, next) {
     const token = req.header("x-auth-token");
     // no token
     if (!token) res.status(401).send("Access denied. No token provided.");
@@ -45,36 +45,36 @@ app.use('/private', function(req, res, next){
     try {
         const decoded = jwt.verify(token, secret);
         req.decoded = decoded;
-    }catch (exception) {
+    } catch (exception) {
         res.status(400).send("Invalid token.");
     }
     next()
 })
 
 app.post('/register', function (req, res) {
-    if(req.body.username && req.body.password && req.body.passQuestion && req.body.passAnswer && req.body.city &&
-        req.body.country && req.body.email && req.body.firstName && req.body.lastName){
-                DButilsAzure.execQuery("INSERT INTO Users VALUES ('" + req.body.username +"', '"+ req.body.password +"', '"+ req.body.passQuestion +"', '"+ req.body.passAnswer +"', '"+ req.body.city +"', '"+
-                    req.body.country +"', '"+ req.body.email +"', '"+ req.body.firstName +"', '"+ req.body.lastName +"');")
-                .then(function (result) {
-                    res.send()
-                })
-                .catch(function (err) {
-                    console.log(err)
-                    res.send(err)
-                })
-        } else {
-            res.status(401).send("Please insert all required fields!");
-        }
+    if (req.body.username && req.body.password && req.body.passQuestion && req.body.passAnswer && req.body.city &&
+        req.body.country && req.body.email && req.body.firstName && req.body.lastName) {
+        DButilsAzure.execQuery("INSERT INTO Users VALUES ('" + req.body.username + "', '" + req.body.password + "', '" + req.body.passQuestion + "', '" + req.body.passAnswer + "', '" + req.body.city + "', '" +
+            req.body.country + "', '" + req.body.email + "', '" + req.body.firstName + "', '" + req.body.lastName + "');")
+            .then(function (result) {
+                res.send()
+            })
+            .catch(function (err) {
+                console.log(err)
+                res.send(err)
+            })
+    } else {
+        res.status(401).send("Please insert all required fields!");
+    }
 
 })
 
 app.post('/private/mostUpdatedPois', function (req, res) {
     var username = req.decoded.username;
-    DButilsAzure.execQuery("SELECT  TOP 2 POI.* FROM POI, FoiOfUsers WHERE FoiOfUsers.IdFOI = POI.IdFOI AND FoiOfUsers.username='"+ username+"' order by POI.Rank DESC")
+    DButilsAzure.execQuery("SELECT  TOP 2 POI.* FROM POI, CategoryOfUsers WHERE CategoryOfUsers.CategoryID = POI.CategoryID AND CategoryOfUsers.username='" + username + "' order by POI.Rank DESC")
         .then(function (result) {
             res.send(result)
-    })
+        })
         .catch(function (err) {
             console.log(err)
             res.send(err)
@@ -83,7 +83,7 @@ app.post('/private/mostUpdatedPois', function (req, res) {
 
 app.post('/private/lastSavedPois', function (req, res) {
     var username = req.decoded.username;
-    DButilsAzure.execQuery("SELECT top 2 POI.* FROM POI, Favorite WHERE Favorite.NamePOI = POI.NamePOI AND Favorite.username='"+ username+"' order by Favorite.indexForUser DESC")
+    DButilsAzure.execQuery("SELECT top 2 POI.* FROM POI, Favorite WHERE Favorite.NamePOI = POI.NamePOI AND Favorite.username='" + username + "' order by Favorite.indexForUser DESC")
         .then(function (result) {
             res.send(result)
         })
@@ -92,3 +92,16 @@ app.post('/private/lastSavedPois', function (req, res) {
             res.send(err)
         })
 })
+
+
+app.post('/sortByCategory', function (req, res) {
+    DButilsAzure.execQuery("SELECT POI.* FROM POI WHERE POI.CategoryID ='" + req.body.categoryID + "'")
+        .then(function (result) {
+            res.send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
+})
+
